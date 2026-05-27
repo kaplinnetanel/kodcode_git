45a43ca367111ef9d39f88d2cbf0a00581cf2776
נתנאל קאפלין <kaplinnetanel10@gmail.com>
2026-05-27 12:49:02 +0300

    Create base Shape class

--- calculator.py/calculattor.py
+++ calculator.py/calculattor.py
@@ -1,7 +1,7 @@
 
 class Shap:  
     def __init__(self): 
-        pass
+        super().__setattr__('is_valid', True)
       
     def get_area(self): 
         pass  
@@ -11,3 +11,11 @@ class Shap:
    
     def __str__(self):    
         return "Generic Shape"
+    
+    def __setattr__(self, name, value):
+        if name != 'is_valid' and isinstance(value, (int, float)):
+            if value <= 0:
+                print("Side must be greater than 0") 
+                super().__setattr__('is_valid', False)
+                raise ValueError("Side must be greater than 0")
+        super().__setattr__(name, value)    
--- calculator.py/main.py
+++ calculator.py/main.py
@@ -6,7 +6,7 @@ from triangle import Triangle
 def main():
     shapes = [
         Rectangle(10, 5),
-        Square(4),
+        Square(6),
         Triangle(6, 4, 5, 6, 5),
         Circle(3),
         Hexagon(5)
