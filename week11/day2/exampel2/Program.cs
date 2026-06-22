dd89951e58b578fe1d5fffa23b225e2c0d076708
נתנאל קאפלין <kaplinnetanel10@gmail.com>
2026-06-22 12:52:00 +0300

    day2

---
+++ week11/day2/exampel2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace exampel2;
+class Example2
+{
+  
+    static List<string> tracks = new List<string>();
+
+    static List<double> speeds = new List<double>();
+
+    static void AddTrack(string id, double speed) 
+    {
+        tracks.Add(id);
+        speeds.Add(speed);
+    }
+                
+    static double AverageSpeed() 
+    {
+        if (speeds.Count == 0)
+        { return 0.0; }
+        double sum = 0;
+        foreach (double s in speeds)
+        { sum += s; }
+        return sum / speeds.Count;
+     
+    }
+    static List<string> FastTracks(double threshold)
+    {
+        List<string> result = new List<string>();
+        for (int i = 0; i < tracks.Count; i++)
+        {
+            if (speeds[i] > threshold)
+                result.Add(tracks[i]);
+        }
+        return result;
+   
+    }
+    List<string> fast = FastTracks(300);
+    Console.WriteLine($"{fast.Count} fast tracks");
+}
+
---
+++ week11/day2/progectDay2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace progectDay2;
+{
+    clss ManagementConsole
+    { 
+        static list <id> ids = new list<ids>
+
+
+
+
+
+
+    }
+
+}
+
+    
